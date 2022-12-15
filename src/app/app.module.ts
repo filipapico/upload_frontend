2417f033f41c0b551db95ac934702190e2afab21
Iuria <betcoiuria@gmail.com>
2022-12-15 12:07:38 +0000

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
