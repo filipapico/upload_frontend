d5cae8b2b054e1a39c9d7c1226561df14f5f3582
filipapico <filipapico@gmail.com>
2022-12-15 12:18:24 +0000

    Created video component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { VideoComponent } from './video/video.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule
