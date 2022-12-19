7aeb035131494b173f11dfe83e089ca4c40c5d6b
Iuria <betcoiuria@gmail.com>
2022-12-19 12:55:29 +0000

    display the app-homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -12,7 +13,8 @@ import { VideoComponent } from './video/video.component';
     VideoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
