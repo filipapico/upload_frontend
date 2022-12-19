cc861f2d1faa0b37ab54f90cfb2b2f7ee7add16a
Iuria <betcoiuria@gmail.com>
2022-12-19 12:54:45 +0000

    import the UploadService + call the getLatestVideos function

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UploadService} from "../upload.service";
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  constructor(private uploadService: UploadService) {
+  }
+  latestVideos: any = [];
+
+  ngOnInit(): void {
+    this.uploadService.getLatestVideos().subscribe((latestVideos:any)=>
+    {
+      this.latestVideos = latestVideos;
+      console.log(this.latestVideos);
+    })
+  }
+
 
 }
