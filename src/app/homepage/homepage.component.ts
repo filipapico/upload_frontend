ddd378ec8acd7058f77f261e2c3a044eba728439
filipapico <filipapico@gmail.com>
2022-12-19 17:33:48 +0000

    Created LatestVideos interface and update it in homepage component. There is still an error that needs to be corrected.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -7,17 +7,17 @@ import {UploadService} from "../upload.service";
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
+  latestVideos: LatestVideos[] = [];
+
   constructor(private uploadService: UploadService) {
   }
-  latestVideos: any = [];
 
   ngOnInit(): void {
-    this.uploadService.getLatestVideos().subscribe((latestVideos:any)=>
-    {
-      this.latestVideos = latestVideos;
+    this.uploadService.getLatestVideos().subscribe((latestVideos) => {
+      // [0] ERROR BELLOW THAT NEEDS CORRECTION. EITHER HERE OR IN INTERFACE DEFINITION...
+      this.latestVideos[0] = latestVideos;
       console.log(this.latestVideos);
     })
   }
-
 
 }
---
+++ src/app/interfaces.ts
@@ -0,0 +1,8 @@
+interface LatestVideos {
+  name: string
+  created: string
+  mid: string
+  field_media_tags: string
+  name_1: string //REMOVE from BACKEND it is repeated
+  thumbnail__target_id: string
+}
