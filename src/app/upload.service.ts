a6fd72af9b398e53f1294da5bfda045b048a2b8b
Iuria <betcoiuria@gmail.com>
2022-12-19 12:49:55 +0000

    created a service + imported httpClient + getLatestVideos()

---
+++ src/app/upload.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+const BASE_URL = "https://dev-project-upskill2-grupo4v2.pantheonsite.io";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService {
+
+  constructor(public http : HttpClient) { }
+
+  getLatestVideos(){
+    return this.http.get(BASE_URL + "/api/latest-videos");
+  }
+
+}
