3a7dea4aa3bee0c204186cbf83a7e826bb79e0d4
Iuria <betcoiuria@gmail.com>
2022-12-27 20:50:20 +0000

    Added the :id to the path playlist

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {HomepageComponent} from "./homepage/homepage.component";
+import {VideoComponent} from "./video/video.component";
+import {PlaylistComponent} from "./playlist/playlist.component";
+
+const routes: Routes = [
+  {path: '', redirectTo: '/homepage', pathMatch: 'full'},
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'video', component: VideoComponent},
+  {path: 'playlist/:id', component: PlaylistComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
