0394da7643ccae35aef592444478a5c756464498
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2023-01-03 21:06:42 +0000

    imported: {ActivatedRoute}, {UploadService} and {DomSanitizer}

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input, } from '@angular/core';
+import {Component, Input,} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+import {ActivatedRoute} from "@angular/router";
+import {UploadService} from "../upload.service";
+
 
 @Component({
   selector: 'app-video',
@@ -13,5 +17,9 @@ export class VideoComponent {
   @Input() field_media_tags!: string;
   @Input() thumbnail__target_id!: string;
   @Input() field_media_oembed_video?: string;
+
+  constructor(private route: ActivatedRoute, private uploadService: UploadService, private sanitizer : DomSanitizer) {
+  }
+
 
 }
