36ded6f3dcab3c475288e47cccfc643a7f3445b4
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2023-01-03 21:05:06 +0000

    renamed LatestVideo interface to Video to make it more generic. Updated properties as well

--- src/app/interfaces.ts
+++ src/app/interfaces.ts
@@ -1,10 +1,13 @@
-interface LatestVideo {
+interface Video {
   name: string
   created: string
   mid: string
   field_media_tags: string
   thumbnail__target_id: string
+  field_duration: string
   field_media_oembed_video: string
+  field_comments: string
+  field_categories: string
 }
 
 interface Playlist {
