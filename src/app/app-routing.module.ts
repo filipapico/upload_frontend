8a16c925662adc8b0429902f9f7ab812c7cf9514
Iuria <betcoiuria@gmail.com>
2023-01-03 11:38:10 +0000

    added the "playlits" route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomepageComponent} from "./homepage/homepage.component";
 import {VideoComponent} from "./video/video.component";
 import {PlaylistComponent} from "./playlist/playlist.component";
+import {PlaylistsComponent} from "./playlists/playlists.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/homepage', pathMatch: 'full'},
   {path: 'homepage', component: HomepageComponent},
   {path: 'video', component: VideoComponent},
+  {path: 'playlists/', component: PlaylistsComponent},
   {path: 'playlist/:id', component: PlaylistComponent},
 ];
 
