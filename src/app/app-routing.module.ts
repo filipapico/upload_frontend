7eff586340a71b0119646e540e00fe9c8bfe4abe
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2023-01-03 22:47:53 +0000

    added {path: 'video-detail/:id', component: VideoDetailComponent},

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {HomepageComponent} from "./homepage/homepage.component";
 import {VideoComponent} from "./video/video.component";
+import {VideoDetailComponent} from "./video-detail/video-detail.component";
 import {PlaylistComponent} from "./playlist/playlist.component";
 import {PlaylistsComponent} from "./playlists/playlists.component";
 
@@ -9,6 +10,7 @@ const routes: Routes = [
   {path: '', redirectTo: '/homepage', pathMatch: 'full'},
   {path: 'homepage', component: HomepageComponent},
   {path: 'video', component: VideoComponent},
+  {path: 'video-detail/:id', component: VideoDetailComponent},
   {path: 'playlists', component: PlaylistsComponent},
   {path: 'playlist/:id', component: PlaylistComponent},
 ];
