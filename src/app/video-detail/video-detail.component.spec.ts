4c7db560875cc92bae62ceb372bd54b1fe373806
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2023-01-03 22:45:05 +0000

    created video-detail.component.

---
+++ src/app/video-detail/video-detail.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VideoDetailComponent } from './video-detail.component';
+
+describe('VideoDetailComponent', () => {
+  let component: VideoDetailComponent;
+  let fixture: ComponentFixture<VideoDetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VideoDetailComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(VideoDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
