b9673c4f7ffc710187cbac9744cd41c99e9cf0ac
filipapico <filipapico@gmail.com>
2023-01-04 11:01:13 +0000

    Added thematic routerLink to menu.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {VideoComponent} from "./video/video.component";
 import {VideoDetailComponent} from "./video-detail/video-detail.component";
 import {PlaylistComponent} from "./playlist/playlist.component";
 import {PlaylistsComponent} from "./playlists/playlists.component";
+import {ThematicComponent} from "./thematic/thematic.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/homepage', pathMatch: 'full'},
   {path: 'homepage', component: HomepageComponent},
   {path: 'video', component: VideoComponent},
   {path: 'video-detail/:mid', component: VideoDetailComponent},
+  {path: 'thematic', component: ThematicComponent},
   {path: 'playlists', component: PlaylistsComponent},
   {path: 'playlist/:id', component: PlaylistComponent},
 ];
