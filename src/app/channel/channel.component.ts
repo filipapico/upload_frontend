00d274f72e82e1dd2623b0affb6d664a8e7059ab
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2023-01-09 19:48:22 +0000

    channelComponent receives snapshot of the clicked channel id, and calls getChannelVideos(id) from the service to receive an array of all the channel's videos

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {UploadService} from "../upload.service";
+import {DomSanitizer} from "@angular/platform-browser";
+import {Channels, Video} from "../interfaces";
 
 @Component({
   selector: 'app-channel',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./channel.component.scss']
 })
 export class ChannelComponent {
+  nid: number; //channel id
+  videos?: Video[] = []; //array containing the channel's videos
+  channelTitle?: string;
+  channelDescription?: string;
+  channelCoverPhoto?: string;
+  channelLogotype?: string;
+
+
+  constructor(private route: ActivatedRoute, public uploadService: UploadService, private sanitizer: DomSanitizer) {
+    this.nid = route.snapshot.params['nid'];
+  }
+
+  ngOnInit(): void {
+    this.uploadService.getChannelVideos(this.nid).subscribe((videos) => {
+      this.videos = videos;
+      this.channelTitle = this.videos[0].title;
+      this.channelDescription = this.videos[0].body;
+      this.channelCoverPhoto = this.videos[0].field_cover_photo;
+      this.channelLogotype = this.videos[0].field_logotype;
+    })
+  }
 
 }
