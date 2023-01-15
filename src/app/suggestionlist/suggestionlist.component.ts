a079c3e8fac07d7cabe77f76f1baba1d5c42e0b4
filipapico <filipapico@gmail.com>
2023-01-15 15:19:38 +0000

    Updated suggestionlist component, including variables import (html, ts) and scss

--- src/app/suggestionlist/suggestionlist.component.ts
+++ src/app/suggestionlist/suggestionlist.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-suggestionlist',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./suggestionlist.component.scss']
 })
 export class SuggestionlistComponent {
+
+  @Input() listTitle!: string
+  @Input() listThumbnail!: string
+  @Input() listElement!:string
 
 }
