0f992ebacd313c8ab13ac4c71627f0375be08446
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2023-01-16 23:55:28 +0000

    in progress: displaying each channel's commentaries

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -10,12 +10,15 @@ import {Video} from "../interfaces";
   styleUrls: ['./channel.component.scss']
 })
 export class ChannelComponent {
-  nid: number; //channel id
+  nid: string; //channel id
   videos?: Video[] = []; //array containing the channel's videos
+  comments?: Comment[] = [];
+  nodeComments?: any;
   channelTitle?: string;
   channelDescription?: string;
   channelCoverPhoto?: string;
   channelLogotype?: string;
+  type: string = "node";
 
 
   constructor(private route: ActivatedRoute, public uploadService: UploadService, private sanitizer: DomSanitizer) {
@@ -25,11 +28,27 @@ export class ChannelComponent {
   ngOnInit(): void {
     this.uploadService.getChannelVideos(this.nid).subscribe((videos) => {
       this.videos = videos;
+      //reaching Channel details from the first video of the channel (not a good idea! if it is empty?
+      //need to work on that
+
       this.channelTitle = this.videos[0].title;
       this.channelDescription = this.videos[0].body;
       this.channelCoverPhoto = this.videos[0].field_cover_photo;
       this.channelLogotype = this.videos[0].field_logotype;
     })
+
+    //getting comments of that specific channel
+/*    this.uploadService.getComments(this.type).subscribe((comments) => {
+      this.comments = comments;
+      this.comments.map((element: any) => {
+        if (element.nid == this.nid)
+          this.nodeComments.push(element);
+      });
+    })*/
+
+    // console.log(this.nodeComments);
+
+
   }
 
 }
