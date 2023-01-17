7f95b352a7d0d750637046b3f33b0418e229b054
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2023-01-17 16:59:55 +0000

    importe Comment interface, calling getComments() from the service and passing "node" and this.nid as arguments to get comments of a specific channel

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {UploadService} from "../upload.service";
 import {DomSanitizer} from "@angular/platform-browser";
-import {Video} from "../interfaces";
+import {Video, Comment} from "../interfaces";
 
 @Component({
   selector: 'app-channel',
@@ -10,15 +10,14 @@ import {Video} from "../interfaces";
   styleUrls: ['./channel.component.scss']
 })
 export class ChannelComponent {
-  nid: string; //channel id
+  nid: number; //channel id
   videos?: Video[] = []; //array containing the channel's videos
-  comments?: Comment[] = [];
-  nodeComments?: any;
+  channelComments?: Comment[] = [];
   channelTitle?: string;
   channelDescription?: string;
   channelCoverPhoto?: string;
   channelLogotype?: string;
-  type: string = "node";
+  channelEntityType = "node";
 
 
   constructor(private route: ActivatedRoute, public uploadService: UploadService, private sanitizer: DomSanitizer) {
@@ -28,27 +27,22 @@ export class ChannelComponent {
   ngOnInit(): void {
     this.uploadService.getChannelVideos(this.nid).subscribe((videos) => {
       this.videos = videos;
-      //reaching Channel details from the first video of the channel (not a good idea! if it is empty?
+      //reaching Channel details from the first video of the channel (not a good idea! what if it is empty?
       //need to work on that
-
       this.channelTitle = this.videos[0].title;
       this.channelDescription = this.videos[0].body;
       this.channelCoverPhoto = this.videos[0].field_cover_photo;
       this.channelLogotype = this.videos[0].field_logotype;
     })
 
-    //getting comments of that specific channel
-/*    this.uploadService.getComments(this.type).subscribe((comments) => {
-      this.comments = comments;
-      this.comments.map((element: any) => {
-        if (element.nid == this.nid)
-          this.nodeComments.push(element);
-      });
-    })*/
-
-    // console.log(this.nodeComments);
+    //getting comments of that specific channel:
+    this.uploadService.getComments("node", this.nid).subscribe((comments) => {
+      this.channelComments = comments;
+      console.log(this.channelComments);
+    })
 
 
   }
+
 
 }
