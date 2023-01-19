224674f7bbba288e38657ff4b96909c671f09eaf
filipapico <filipapico@gmail.com>
2023-01-19 15:23:28 +0000

    Installed locally gravatar directive

---
+++ src/app/gravatar.directive.spec.ts
@@ -0,0 +1,8 @@
+import { GravatarDirective } from './gravatar.directive';
+
+describe('GravatarDirective', () => {
+  it('should create an instance', () => {
+    const directive = new GravatarDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/gravatar.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appGravatar]'
+})
+export class GravatarDirective {
+
+  constructor() { }
+
+}
