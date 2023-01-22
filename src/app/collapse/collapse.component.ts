582d838e11843fcbe2085f7003ad3473a8490c90
filipapico <filipapico@gmail.com>
2023-01-22 19:40:38 +0000

    Started working on a component that will be used as a collapse sidebar (work in progress).

--- src/app/collapse/collapse.component.ts
+++ src/app/collapse/collapse.component.ts
@@ -1,12 +1,19 @@
 import {Component} from '@angular/core';
 import {faBars} from "@fortawesome/free-solid-svg-icons";
+import {CardModule, CollapseModule} from "@coreui/angular";
 
 @Component({
   selector: 'app-collapse',
   templateUrl: './collapse.component.html',
   styleUrls: ['./collapse.component.scss']
 })
-export class CollapseComponent {
-  faBars = faBars
 
+export class CollapseComponent {
+  faBars = faBars;
+  visible: boolean = false;
+
+  toggleCollapse(): void {
+    this.visible = !this.visible;
+    console.log("this visible",this.visible)
+  }
 }
