d85db7527b19fb6c011ff827905ea5a6a2d2b2e8
filipapico <filipapico@gmail.com>
2023-01-22 12:56:14 +0000

    Created collapse component to be used in mobile menu

---
+++ src/app/collapse/collapse.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+
+@Component({
+  selector: 'app-collapse',
+  templateUrl: './collapse.component.html',
+  styleUrls: ['./collapse.component.scss']
+})
+export class CollapseComponent {
+  faBars = faBars
+
+}
