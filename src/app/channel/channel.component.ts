dbe5436ff1da5a8c1cf433d64d267fb096a52abd
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2023-01-22 17:29:56 +0000

    onInit, calling getChannels and passing current nid as argument, to get channel infos (cover photo, logotype etc)

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {UploadService} from "../upload.service";
 import {DomSanitizer} from "@angular/platform-browser";
-import {Video, Comment} from "../interfaces";
+import {Video, Comment, Channels} from "../interfaces";
 import {faBookmark} from '@fortawesome/free-regular-svg-icons';
 import {faBookmark as faBookmarkFull} from "@fortawesome/free-solid-svg-icons";
 
@@ -14,15 +14,11 @@ import {faBookmark as faBookmarkFull} from "@fortawesome/free-solid-svg-icons";
 export class ChannelComponent {
   faBookmark = faBookmark;
   faBookmarkFull = faBookmarkFull;
-
   nid: number; //channel id
   videos?: Video[] = []; //array containing the channel's videos
   channelComments?: Comment[] = [];
-  channelTitle?: string;
-  channelDescription?: string;
-  channelCoverPhoto?: string;
-  channelLogotype?: string;
   channelEntityType = "node";
+  channel?: Channels[] = [];
 
 
   constructor(private route: ActivatedRoute, public uploadService: UploadService, private sanitizer: DomSanitizer) {
@@ -30,24 +26,17 @@ export class ChannelComponent {
   }
 
   ngOnInit(): void {
+    this.uploadService.getChannels(this.nid.toString()).subscribe((channel) => {
+      this.channel = channel;
+    });
+
     this.uploadService.getChannelVideos(this.nid).subscribe((videos) => {
       this.videos = videos;
-      //reaching Channel details from the first video of the channel (not a good idea! what if it is empty?
-      //need to work on that
-      this.channelTitle = this.videos[0].title;
-      this.channelDescription = this.videos[0].body;
-      this.channelCoverPhoto = this.videos[0].field_cover_photo;
-      this.channelLogotype = this.videos[0].field_logotype;
-    })
+    });
 
     //getting comments of that specific channel:
     this.uploadService.getComments("node", this.nid).subscribe((comments) => {
       this.channelComments = comments;
-      console.log(this.channelComments);
     })
-
-
   }
-
-
 }
