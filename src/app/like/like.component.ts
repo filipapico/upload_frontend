19faf96f1f5d76028ca3b824631586162413fd49
filipapico <filipapico@gmail.com>
2023-01-23 12:45:37 +0000

    Updated like component html and ts files (WIP...)

--- src/app/like/like.component.ts
+++ src/app/like/like.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {faThumbsUp as faThumbsUpSolid} from "@fortawesome/free-solid-svg-icons";
 import {faThumbsUp} from "@fortawesome/free-regular-svg-icons";
 import {faThumbsDown as faThumbsDownSolid} from "@fortawesome/free-solid-svg-icons";
 import {faThumbsDown} from "@fortawesome/free-regular-svg-icons";
+import {UploadService} from "../upload.service";
 
 @Component({
   selector: 'app-like',
@@ -14,4 +15,20 @@ export class LikeComponent {
   faThumbsUp = faThumbsUp;
   faThumbsUpSolid = faThumbsUpSolid;
   faThumbsDown = faThumbsDown;
+
+  @Input () id_video!: string
+
+  body: {} = {
+    "entity_id":["51"],
+    "entity_type":["media"],
+    "flag_id":[{"target_id": "like_videos","target_type": "flag"}],
+    "uid": ["0"]
+  }
+
+  constructor(private uploadService: UploadService) {
+  }
+
+  addlikeToVideo(id:string) {
+    console.log(id)  }
+
 }
