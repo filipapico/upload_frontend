f1b9a5522f219018e71d34ae7c3330ad101918e8
filipapico <filipapico@gmail.com>
2023-01-26 13:00:45 +0000

    Updated videotag component

--- src/app/videotag/videotag.component.ts
+++ src/app/videotag/videotag.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UploadService} from "../upload.service";
+import {Video} from "../interfaces";
 
 @Component({
   selector: 'app-videotag',
   templateUrl: './videotag.component.html',
   styleUrls: ['./videotag.component.scss']
 })
-export class VideotagComponent {
+export class VideotagComponent implements OnInit {
+  videosByTag!: Video[]
+
+  constructor(private uploadService: UploadService) {
+  }
+
+  ngOnInit() {
+    this.uploadService.getVideosByTag('3', 0).subscribe((videosByTag) => {
+      this.videosByTag = videosByTag
+      console.log(videosByTag)
+    })
+  }
 
 }
