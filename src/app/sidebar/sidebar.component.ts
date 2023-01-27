01e57b1ec5eca68bd9f0bdb70bcdf761c93f1144
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2023-01-27 16:49:55 +0000

    imported uploadService

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {UploadService} from "../upload.service";
 
 @Component({
   selector: 'app-sidebar',
@@ -6,4 +7,8 @@ import {Component} from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
+  constructor(public uploadService: UploadService) {
+  }
+
+
 }
