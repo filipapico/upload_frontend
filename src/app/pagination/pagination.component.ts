6c8989a2663f320fbb6ecc47be031d6442db122f
filipapico <filipapico@gmail.com>
2023-01-30 22:02:53 +0000

    Unfruitful attempts to use pagination component.

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,3 +1,4 @@
+
 import {Component, Input} from '@angular/core';
 import {UploadService} from "../upload.service";
 import {Tags, Thematics} from "../interfaces";
@@ -8,17 +9,35 @@ import {Tags, Thematics} from "../interfaces";
   styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent {
-  thematics!: Thematics[]
 
-  @Input() list!: Tags[]
+  @Input() pageNumber: number = 0
+  @Input() list!: string[]
+  @Input() type!: string
 
   constructor(private uploadService: UploadService) {
   }
+}
+/*
+getLess(pageNumber: number) {
 
-  getThematicsByTag(id: string) {
-    this.uploadService.getThematicsByTag(id).subscribe((thematicTag) => {
-      this.thematics = thematicTag
-    })
+}
+
+getMore(p: number) {
+  if (this.list.length <= 10) {
+    p++
+    this.pageNumber = p
+    if (this.type == "thematics")
+      this.uploadService.getTagsInThematics(p).subscribe((tags) => {
+        this.list = tags
+      })
   }
 
 }
+
+getMoreTags(p: number) {
+
+}
+
+
+}
+*/
