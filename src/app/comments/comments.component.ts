3ff4be130241a0942f8bc4a8fce44ae416557e29
Iuria <betcoiuria@gmail.com>
2023-01-30 15:02:30 +0000

    added a new icon (close - faTimesCircle), function to open and close popup, function that post report

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from '@angular/core';
-import {faFlag} from "@fortawesome/free-regular-svg-icons";
+import {faFlag, faTimesCircle} from "@fortawesome/free-regular-svg-icons";
 import {HttpHeaders} from "@angular/common/http";
 import {UploadService} from "../upload.service";
 import {Comment} from "../interfaces";
@@ -12,6 +12,9 @@ import {Comment} from "../interfaces";
 })
 export class CommentsComponent {
   faFlag = faFlag;
+  faTimesCircle = faTimesCircle;
+  index_cid : number = 0;
+
 
   @Input() gravatar?: string;
   @Input() username?: string;
@@ -28,6 +31,7 @@ export class CommentsComponent {
 
   constructor(public uploadService: UploadService) {
   }
+
 
   commentUrl = 'https://dev-project-upskill2-grupo4v2.pantheonsite.io/comment';
   tokenValue?: any;
@@ -84,5 +88,39 @@ export class CommentsComponent {
     this.uploadService.getComments(this.entityType, this.contentID).subscribe((comments) => {
       this.entityComments = comments;
     })
+  }
+
+
+
+  show = false;
+
+  Openpopup(index: any, cid: string) {
+    this.show = true;
+    let index_cid = index
+    console.log (index_cid)
+    console.log(cid)
+    let reason = document.querySelector("#field_reason")
+
+   let bodyReport: {} = {
+      "entity_id": [cid],
+      "entity_type": ["comment"],
+      "field_reason": [reason],
+      "flag_id": [{"target_id": "report_comments", "target_type": "flag"}],
+      "uid": ["0"]
+    }
+  }
+
+  addCommentReport(urlReport: string, bodyReport: {}, headersReport: any) {
+    let reportHeaders = new HttpHeaders({
+      'X-CSRF-Token': 'VQrAx6wI4cv-J3BdqLRhIbN5gfUUCGf9sZnR_teei2U',
+      'Accept': 'application/vnd.api+json'
+    });
+
+    this.uploadService.postReport(urlReport, bodyReport, reportHeaders).subscribe((data) => {
+    })
+  }
+
+  Closepopup() {
+    this.show = false
   }
 }
