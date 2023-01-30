7d75427031a379cac2939cf80e824ea36d83e515
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2023-01-30 22:48:27 +0000

    as suggestions of videos in right sidebar, implemented a call to api that returns 4 last videos of the same channel (getChannelVideos(parseInt(this.video.nid))

--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {UploadService} from "../upload.service";
-import {Video, Comment} from "../interfaces";
+import {Video} from "../interfaces";
 import {DomSanitizer} from "@angular/platform-browser";
 import {faBookmark} from '@fortawesome/free-regular-svg-icons';
 import {faBookmark as faBookmarkFull} from "@fortawesome/free-solid-svg-icons";
@@ -14,13 +14,12 @@ import {faBookmark as faBookmarkFull} from "@fortawesome/free-solid-svg-icons";
 export class VideoDetailComponent implements OnChanges {
   faBookmark = faBookmark;
   faBookmarkFull = faBookmarkFull;
-
   id!: number;
   video?: Video;
   video_url?: any;
-  latestVideosList: Video[] = [];
   mediaEntityType = "media";
-
+  channelVideos?: Video[] = []; //array containing the channel's videos
+  nid?: number;
 
   @Input() id_video?: string
   @Input() type?: string;
@@ -44,21 +43,18 @@ export class VideoDetailComponent implements OnChanges {
         this.id = params['mid']
         this.refresh()
       });
-
     }
   }
 
   refresh() {
     this.uploadService.getVideo(this.id).subscribe((video) => {
       this.video = video[0];
+      this.uploadService.getChannelVideos(parseInt(this.video.nid)).subscribe((videos) => {
+        this.channelVideos = videos;
+      });
 
       this.video_url = this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/" + this.video.field_media_oembed_video.split("?v=")[1]);
       return this.video_url;
-
-    })
-
-    this.uploadService.getLatestVideos(0).subscribe((latestVideos) => {
-      this.latestVideosList = latestVideos;
     })
   }
 
