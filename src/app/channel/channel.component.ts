22113e6ef3b79457fd571660a10411cc8afd4731
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2023-02-04 16:58:04 +0000

    checking if this.nid is defined before transforming to string and getting channels, to resolve error on the console

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -10,40 +10,40 @@ import {Video, Channels} from "../interfaces";
 })
 export class ChannelComponent {
 
-  nid: number; //channel id
+  nid!: number; //channel id
   videos?: Video[] = []; //array containing the channel's videos
   channelEntityType = "node";
-  channel?: Channels[] = [];
+  channel!: Channels[];
 
 
   constructor(private route: ActivatedRoute, public uploadService: UploadService) {
     this.nid = route.snapshot.params['nid'];
   }
 
-  refresh(){
-    this.uploadService.getChannels(this.nid.toString()).subscribe((channel) => {
-      this.channel = channel;
-    });
+  refresh() {
+    if (this.nid) {
+      let nid = this.nid.toString();
+      this.uploadService.getChannels(nid).subscribe((channel) => {
+        this.channel = channel;
+      });
 
-    this.uploadService.getChannelVideos(this.nid).subscribe((videos) => {
-      this.videos = videos;
-    });
+      this.uploadService.getChannelVideos(nid).subscribe((videos) => {
+        this.videos = videos;
+      });
+    }
   }
 
   ngOnInit(): void {
     this.uploadService.onChangeLanguage(() => {
       this.refresh();
     });
-    this.route.params.subscribe((params)=>{
+    this.route.params.subscribe((params) => {
       let slug = params['name']
-      this.uploadService.getContentBySlug("channel", slug).subscribe((data:any)=>{
-        this.nid=data.nid[0].value
+      this.uploadService.getContentBySlug("channel", slug).subscribe((data: any) => {
+        this.nid = data.nid[0].value;
         this.refresh();
       })
     })
-
-
-    this.refresh();
   }
 
   getHashtag(tags: string) {
