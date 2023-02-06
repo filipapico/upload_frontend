80f0de1414504c3d3dfa7a0bb39291823785bba9
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2023-02-06 17:18:40 +0000

    added translated strings to the ts files and importing values in html docs

--- src/locale/en.ts
+++ src/locale/en.ts
@@ -3,13 +3,21 @@ interface Traducao {
 }
 
 const en: Traducao = {
+  "all channels": "All Channels",
+  "by": "by",
   "channels": "Channels",
   "english": "English",
   "favorites": "Favorites",
   "home": "Home",
+  "leave-comment": "Feel free to leave us a comment!",
+  "no video": "Please be patient, this channel will upload videos soon!",
   "playlists": "Playlists",
   "portuguese": "Português",
-  "thematics": "Thematics"
+  "submit": "Submit",
+  "report-submitted": "Your report was submitted!",
+  "thematics": "Thematics",
+  "url": "URL Copied!",
+  "videos": "Videos"
 }
 
 export default en;
--- src/locale/pt.ts
+++ src/locale/pt.ts
@@ -3,13 +3,21 @@ interface Traducao {
 }
 
 const pt: Traducao = {
+  "all channels": "Mais canais",
+  "by": "por",
   "channels": "Canais",
   "english": "English",
   "favorites": "Favoritos",
   "home": "Início",
+  "leave-comment": "Sinta-se à vontade para nos deixar um comentário!",
+  "no video": "Por favor, seja paciente, este canal enviará vídeos em breve!",
   "playlists": "Playlists",
   "portuguese": "Português",
-  "thematics": "Temáticas"
+  "submit": "Submeter",
+  "report-submitted": "Sua denúncia foi enviada!",
+  "thematics": "Temáticas",
+  "url": "URL Copiado!",
+  "videos": "Vídeos"
 }
 
 export default pt;
