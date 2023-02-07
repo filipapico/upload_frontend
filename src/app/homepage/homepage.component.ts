45ba334ad7dbcc65fc0757cad9f668f97cae9a19
filipapico <filipapico@gmail.com>
2023-02-07 21:55:24 +0000

    Implemented full pager pagination in homepage. Older code is still commented, to be deleted.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -20,35 +20,37 @@ export class HomepageComponent implements OnInit {
 
   public keyword = 'name';
   data = this.allVideos;
+  pnumVideos: number = 0
+  pNumsVideos!: number[];
+  videosPerPage: number = 10 //needs to be according to pagination in the view/rest export set in DRUPAL
 
   constructor(public uploadService: UploadService) {
   }
 
-  refresh() {
-    this.getVideos();
+  ngOnInit(): void {
+    this.uploadService.onChangeLanguage(() => {
+      this.refresh(this.pnumVideos);
+    });
+    this.refresh(this.pnumVideos);
+  }
+
+  refresh(pageVideos: number) {
+    this.pnumVideos = pageVideos
+    this.uploadService.getPagination
+    ("latest-videos", '', this.videosPerPage).subscribe(({
+                                                           pageNumbers
+                                                         }) => {
+      this.pNumsVideos = pageNumbers
+    })
+    this.getVideos(pageVideos)
+
     this.uploadService.getThematicsByTag("",0).subscribe((thematics) => {
       this.thematics = thematics;
     })
   }
 
-  ngOnInit(): void {
-    this.uploadService.onChangeLanguage(() => {
-      this.refresh();
-    });
-    this.refresh();
-  }
-
-  pageSelected(selected: any) {
-    if (selected == 'Previous' && this.pNum > 0)    //if prev is clicked, numbers will decrease by 1
-      this.pNum--;
-    else if (selected == 'Next' && this.latestVideos?.length == 10)   //if next is clicked numbers will increase by 1
-      this.pNum++;
-
-    this.getVideos();
-  }
-
-  getVideos() {
-    this.uploadService.getLatestVideos(this.pNum).subscribe((latestVideos) => {
+  getVideos(page: number) {
+    this.uploadService.getLatestVideos(page).subscribe((latestVideos) => {
       this.latestVideos = latestVideos;
     })
     // COMMENTED code since it's not being used - Thematics ar collected in refresh function
@@ -70,3 +72,13 @@ export class HomepageComponent implements OnInit {
     })
   }
 }
+
+//OLD PAGINATIOM
+/*pageSelected(selected: any) {
+  if (selected == 'Previous' && this.pNum > 0)    //if prev is clicked, numbers will decrease by 1
+    this.pNum--;
+  else if (selected == 'Next' && this.latestVideos?.length == 10)   //if next is clicked numbers will increase by 1
+    this.pNum++;
+
+  this.getVideos();
+}*/
